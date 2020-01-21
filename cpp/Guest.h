e0570257acf01c2dea4dd6eabb97031637da9c99
Zerg <amcfarlane52@gmail.com>
2020-01-21 12:48:46 -0800

    renamed header extensions

---
+++ cpp/Guest.h
@@ -0,0 +1,19 @@
+#ifndef GUEST_H
+#define GUEST_H
+
+#include "Person.h"
+
+class Guest : public Person
+{
+private:
+public:
+};
+
+#endif // GUEST_H
+
+/**
+ * TODO: design the Status enum/class for Party and Worker.
+ * TODO: design the Activity class that will use Status.
+ * 
+ * NOTE: Can we use OOP at all here? (i.e. GuestStatus, WorkerActivity, etc)
+ * */
