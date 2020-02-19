f7f824616b432352c1103102f16df6c4dbc33af9
Zerg <amcfarlane52@gmail.com>
2020-02-19 13:13:59 -0800

    removed uncopyable and const accessor tests

--- cpp/tests/src/Person_T.cpp
+++ cpp/tests/src/Person_T.cpp
@@ -6,7 +6,7 @@
  * Unit test for Person.h
  * author: ajm
  * created: 1/30/20
- * last modified: 1/31/20
+ * last modified: 2/19/20
  **/
 
 BOOST_AUTO_TEST_SUITE(person_test_suite)
@@ -23,19 +23,6 @@ BOOST_AUTO_TEST_CASE(case2)
     Person p("Some id");
     BOOST_CHECK(&p);
     BOOST_TEST(p.getId() == "Some id");
-}
-
-BOOST_AUTO_TEST_CASE(case3)
-{ //const Person (testing const accessor)
-    //NOTE: Should this have an error catch here?
-    const Person p("Some id");
-    BOOST_TEST(p.getId() == "Some id");
-}
-
-BOOST_AUTO_TEST_CASE(case4)
-{ //ensuring that Person cannot be copied
-    BOOST_TEST(std::is_copy_constructible<Person>::value == 0);
-    BOOST_TEST(std::is_copy_assignable<Person>::value == 0);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
