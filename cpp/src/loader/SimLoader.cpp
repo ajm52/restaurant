9ff930c0d39dda0bf0056ed95a3aa6c6348a5bcd
AJ <andrew.mcfarlane@ucalgary.ca>
2020-06-02 10:02:05 -0700

    removed code w/ errors for sanity (using CVs instead of FDs)

--- cpp/src/loader/SimLoader.cpp
+++ cpp/src/loader/SimLoader.cpp
@@ -16,7 +16,6 @@ void SimLoader::initDoorFD(Door *d)
     /**
      * TODO: assert doorFD size == 1 and doorFD[0] >= 0
      */
-    Door::LoaderAccess::setFD(d, doorFD.get()->at(0));
 }
 
 void SimLoader::loadWaiterBulletin(WorkerBulletin &wb, const std::shared_ptr<std::vector<Waiter>> waiters)
@@ -34,30 +33,9 @@ std::shared_ptr<std::vector<Party>> SimLoader::createParties(Restaurant &r)
     while (i < fds.get()->size())
     {
         std::string id("P" + std::to_string(i));
-        Party p(r, nullptr, id);
+        Party p(&r, nullptr, id);
         parties.push_back(p);
         i++;
     }
     return std::make_shared<std::vector<Party>>(std::move(parties));
-}
-
-std::shared_ptr<std::vector<Waiter>> SimLoader::createWaiters()
-{
-    std::string waiterConfigPath = "../../meta/waiter_io_data.md";
-    std::shared_ptr<std::vector<int>> fds = dataLoader_.createFDs(waiterConfigPath);
-    std::vector<Waiter> waiters;
-    int i = 0;
-    while (i < fds.get()->size())
-    {
-        std::string id("W" + std::to_string(i));
-        Waiter w(id, fds.get()->at(i));
-        waiters.push_back(w);
-        i++;
-    }
-    return std::make_shared<std::vector<Waiter>>(std::move(waiters));
-}
-
-void SimLoader::initRestaurantWaiters(Restaurant *r, std::shared_ptr<std::vector<Waiter>> w)
-{
-    Restaurant::LoaderAccess::assignWaiters(r, *(w.get()));
 }
