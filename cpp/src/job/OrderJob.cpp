a398d70684328174362f2635a89772b966f3cf6d
AJ <andrew.mcfarlane@ucalgary.ca>
2020-06-10 09:44:55 -0700

    deleted job .cpp files

--- cpp/src/job/OrderJob.cpp
+++
@@ -1,9 +0,0 @@
-#include "OrderJob.h"
-#include "Waiter.h"
-#include <iostream>
-
-void OrderJob::handleJob(Waiter *w)
-{
-    if (w)
-        std::cout << w->getId() << " has begun OrderJob " << this->order_.getOrderId() << std::endl;
-}
