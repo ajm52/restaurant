2f16ba610fec068bedb48e676a947f7b17f42d12
AJ <andrew.mcfarlane@ucalgary.ca>
2020-06-17 09:58:32 -0700

    cleaned up print statements; added pseudocode for next chunk of code in run()

--- cpp/src/people/Party.cpp
+++ cpp/src/people/Party.cpp
@@ -13,7 +13,7 @@
 #include <memory>
 #include <chrono>
 
-void Party::WaiterAccess::signalServiceStarted(Party *p)
+void Party::WaiterAccess::signalServiceStarted(std::shared_ptr<Party> p)
 {
     p->hasBeenServiced_ = true;
     p->cv_.notify_one();
@@ -70,14 +70,20 @@ void Party::init()
 
 void Party::run()
 {
-    std::cout << "Party#" << this->getPID() << " says hello.\n";
+    std::cout << this->getPID() << " is awake.\n";
     enterRestaurant();
-    //awaitService();
+    awaitService();
+    /**
+     * 1. after a pause, place an Order on the Table and submit an OrderJob to the JobTable.
+     * 2. wait until Waiter notifies that it has picked up the Order.
+     * 3. wait again until the Waiter notifies that the Order has been placed on the Table.
+     * 4. run a Timer. 
+     * 5. Leave the Table, and exit the restaurant. 
+     */
 }
 
 void Party::enterRestaurant()
 {
-    std::cout << "here@@\n";
     { // Begin critical section
         while (true)
         {
@@ -87,7 +93,7 @@ void Party::enterRestaurant()
                 theRestaurant_.getDoor().getEntryQueue().push(std::make_shared<Party>(std::move(*this)));
                 break;
             }
-            catch (const std::exception &e)
+            catch (const std::exception &e) //TODO clean up this error handling (eventually)
             {
                 std::cerr << e.what() << "; trying again in 3 seconds.\n";
                 std::this_thread::sleep_for(std::chrono::seconds(3));
@@ -95,15 +101,20 @@ void Party::enterRestaurant()
         }
 
     } // End critical section
-    std::cout << this->getPID() << " has entered the queue.\n";
+
+    std::cout << this->getPID() << " entered the restaurant queue.\n";
 }
+
 void Party::awaitService()
 {
-    theRestaurant_.getDoor().getCV().notify_one();
+    std::cout << this->getPID() << ": Pinging for service...\n";
+    theRestaurant_.getDoor().getCV().notify_one(); // notifies a thread to place this in the Foyer, which notifies a Waiter to place this at a Table.
     std::unique_lock<std::mutex> ul(m_);
-    while (!this->checkServiceFlag())
+
+    while (checkServiceFlag()) // waits here until notified by their Waiter.
         cv_.wait(ul);
-    std::cout << this->getPID() << " has been serviced!\n";
+
+    std::cout << this->getPID() << ": Service notification received.\n"; // at this point, this Party should have access to its Waiter, Table, and Menu.
 }
 
 std::shared_ptr<Party> Party::makeParty(Restaurant &r, unsigned gCount, std::string pid)
