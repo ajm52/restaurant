c14bef494b905befc233cbd131a2762eded746f7
AJ <andrew.mcfarlane@ucalgary.ca>
2020-06-17 08:56:50 -0700

    fixed comment formatting; defaulted dtor

--- cpp/headers/menu/Menu.hpp
+++ cpp/headers/menu/Menu.hpp
@@ -8,71 +8,71 @@
 #include <string>
 
 /**
- * @class <code>Menu</code>
+ * @class Menu
  * @description: The restaurant menu, built from metadata.
  * @author ajm
- * @created: 2/5/20
- * @modified: 2/19/20
  **/
 class Menu
 {
 public:
     /**
-     * @description: main constructor.
-     * @param opts <code>Menu</code> selections.
+     * @description: constructor.
+     * @param opts menu selections.
      **/
     Menu(std::map<MenuEntryKey, MenuEntry, MenuEntryKeyComparator> &);
 
     /**
-     * @description: <code>Menu</code> destructor.
+     * @description: destructor.
      **/
-    ~Menu() {}
+    ~Menu() = default;
 
     /**
-     * @description: deleted copy constructor.
-     * The restaurant only has one <code>Menu</code>, which is shared amongst workers and guests.
+     * @description: copy constructor.
+     * @param m menu being copied.
      **/
     Menu(const Menu &);
 
     /**
-     * @description: deleted copy assignment operator.
+     * @description: copy assignment operator.
+     * @param m menu being copied.
+     * @returns a reference to this menu.
      **/
     Menu &operator=(const Menu &);
 
     /**
-     * @description: selects a <code>Menu</code> option at random.
+     * @description: selects a Menu option at random.
      * @param type type of entry to be randomly selected.
      * @returns name of the selected entry.
      **/
     std::string selectOption(char);
 
     /**
-     * @description: accessor for <code>this Menu</code>'s options.
-     * @returns a <code>const</code> reference to <code>this Menu</code>'s options.
+     * @description: accessor for the menu's options.
+     * @returns a const reference to the menu's options.
      **/
     inline const std::map<MenuEntryKey, MenuEntry, MenuEntryKeyComparator> &getOptions() const { return options_; }
 
     /**
-     * @description: accessor for <code>this Menu</code>'s food option count.
-     * @returns a <code>const</code> copy of <code>this Menu</code>'s food option count.
+     * @description: accessor for the menu's food option count.
+     * @returns a count of the menu's food options.
      **/
     inline const unsigned getNumFoodOptions() const { return countOptions('F'); }
 
     /**
-     * @description: accessor for <code>this Menu</code>'s drink option count.
-     * @returns a <code>const</code> copy of <code>this Menu</code>'s drink option count.
+     * @description: accessor for the menu's drink option count.
+     * @returns a count of the menu's drink options.
      **/
     inline const unsigned getNumDrinkOptions() const { return countOptions('D'); }
 
     /**
-     * @description: Counts the number of options of a certain type in <code>this Menu</code>.
+     * @description: Counts the number of options of a certain type in this menu.
      * @param type type of entries we are counting.
      * @returns the umber of menu entries of a given type.
      */
     const unsigned countOptions(char) const;
 
 private:
-    std::map<MenuEntryKey, MenuEntry, MenuEntryKeyComparator> options_; ///< a table of <code>Menu</code> options.
+    std::map<MenuEntryKey, MenuEntry, MenuEntryKeyComparator> options_; ///< a table of menu options.
 };
 
 /**
