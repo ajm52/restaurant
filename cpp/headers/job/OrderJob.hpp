ca30f52c1590e50503d5075a400965857d133062
AJ <andrew.mcfarlane@ucalgary.ca>
2020-06-17 08:51:04 -0700

    accept(Worker) now takes a worker reference

--- cpp/headers/job/OrderJob.hpp
+++ cpp/headers/job/OrderJob.hpp
@@ -10,13 +10,11 @@
  * @description: A job associated with
  * a restaurant order.
  * @author ajm
- * @created: 5/29/20
- * @modified: 6/3/20
  */
 struct OrderJob : public Job
 {
     OrderJob(Order o) : order_(o) {}
-    void accept(Worker &w) { w.handleJob(this); }
+    void accept(Worker &w) { w.handleJob(*this); }
     Order order_;
 };
 #endif // ORDERJOB_HPP
--- cpp/headers/job/SeatingJob.hpp
+++ cpp/headers/job/SeatingJob.hpp
@@ -8,8 +8,6 @@
  * @struct SeatingJob 
  * @description: A job to do with seating a Party.
  * @author ajm
- * @created: 5/29/20
- * @modified: 6/3/20
  */
 struct SeatingJob : public Job
 {
@@ -18,7 +16,7 @@ struct SeatingJob : public Job
      * @param tID table ID where the party shall be seated.
      */
     SeatingJob(unsigned tID) : tableID_(tID) {}
-    void accept(Worker &w) { w.handleJob(this); }
+    void accept(Worker &w) { w.handleJob(*this); }
     unsigned tableID_; ///< used to index into Foyer's Party map.
 };
 #endif // SEATINGJOB_HPP
