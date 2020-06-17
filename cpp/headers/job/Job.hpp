de49e9c8c3e1a57fbf1826a0b2078d08186115a7
AJ <andrew.mcfarlane@ucalgary.ca>
2020-06-17 08:48:58 -0700

    made Job pure virtual for proper dynam RT Job identification

--- cpp/headers/job/Job.hpp
+++ cpp/headers/job/Job.hpp
@@ -12,20 +12,19 @@
  * An OrderJob will have an attached Order, 
  * and both SeatingJobs and CleaningJobs will 
  * have an attached Table ID.
+ * 
  * @author ajm
- * @created: 5/29/20
- * @modified: 6/3/20
  */
 struct Job
 {
     /**
-     * @description: virtual handler method to be implemented 
+     * @description: pure virtual handler method to be implemented 
      * by concrete Job structs.
      * @param w the worker responsible for the job.
      * 
      * NOTE We avoid pure virtual to simplify
      * generic usage in JobTable.
      */
-    virtual void accept(Worker &) = 0;
+    virtual void accept(Worker &w) = 0;
 };
 #endif // JOB_HPP
