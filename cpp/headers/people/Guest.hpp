6d3ed14a4906afb68d83f58851c7fa490b6df37b
AJ <andrew.mcfarlane@ucalgary.ca>
2020-06-17 09:52:25 -0700

    cleaned up comment syntax

--- cpp/headers/people/Guest.hpp
+++ cpp/headers/people/Guest.hpp
@@ -4,38 +4,36 @@
 #include "Person.hpp"
 
 /**
- * Represents a restaurant guest (inherits from Person)
- * 
- * author: ajm
- * created: 1/28/20
- * last modified: 5/25/20
+ * @description: Represents a restaurant guest (inherits from Person)
+ * @author ajm
  **/
 
 class Guest : public Person
 {
 public:
     /**
-     * default Guest ctor.
-     * @param id Guest id
+     * @description: constructor.
+     * @param id guest id
      */
     Guest(std::string id = "DEFAULT_GID") : Person(id) {}
 
     /**
-     * copy ctor.
-     * @param g Guest to copy from.
+     * @description: copy constructor.
+     * @param g guest to copy from.
      */
     Guest(const Guest &);
 
     /**
-     * copy assignment operator.
-     * @param g Guest to copy from.
+     * @description: copy assignment operator.
+     * @param g guest to copy from.
+     * @returns a reference to this guest.
      */
     Guest &operator=(const Guest &);
 
     /**
-     * destructor.
+     * @description: destructor.
      */
-    ~Guest() {}
+    ~Guest() = default;
 
     // will eventually have methods for choosing food/drink
 };
@@ -43,9 +41,5 @@ public:
 #endif // GUEST_HPP
 
 /**
- * TODO: write the Menu class.
  * TODO: write member functions for selecting food/drink from a given Menu.
- * TODO: design the Activity class that will use Status.
- * 
- * NOTE: Can we use OOP at all here? (i.e. GuestStatus, WorkerActivity, etc)
- * */
+ **/
