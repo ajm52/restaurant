9e1f2971af7428fdb619e8db48fbd4df0c3f614f
AJ <andrew.mcfarlane@ucalgary.ca>
2020-06-29 14:04:22 -0700

    wrapper func for mutex/cv/jobQ/workFlag

---
+++ cpp/headers/contain/WorkerJobData.hpp
@@ -0,0 +1,25 @@
+#ifndef WORKERJOBDATA_HPP
+#define WORKERJOBDATA_HPP
+
+#include <mutex>
+#include <condition_variable>
+#include <queue>
+#include <memory>
+
+struct Job;
+
+/**
+ * @struct WorkerJobData
+ * @author ajm
+ * @description: A struct to be used as a map value in JobTable.
+ * Contains data related to assigning/acquiring work.
+ */
+struct WorkerJobData
+{
+    WorkerJobData() : workToBeDone_(false), m_(), cv_(), jobs_() {}
+    mutable bool workToBeDone_;             ///< worker job flag.
+    mutable std::mutex m_;                  ///< worker mutex.
+    mutable std::condition_variable cv_;    ///< worker condition variable.
+    std::queue<std::shared_ptr<Job>> jobs_; ///< worker job queue.
+};
+#endif // WORKERJOBDATA_HPP
