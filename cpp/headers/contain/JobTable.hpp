92404e70e2c5c2f99cc5cf2d0a1f666ee0d11f21
AJ <andrew.mcfarlane@ucalgary.ca>
2020-06-29 14:03:41 -0700

    JobTable now keyed on std::string; wrote key validation func; mem data reduced to a single std::map holding WorkerJobData values

--- cpp/headers/contain/JobTable.hpp
+++ cpp/headers/contain/JobTable.hpp
@@ -1,13 +1,14 @@
 #ifndef JOBTABLE_HPP
 #define JOBTABLE_HPP
 
-#include "Job.hpp"
+#include "WorkerJobData.hpp"
 #include <vector>
-#include <queue>
 #include <mutex>
 #include <condition_variable>
 #include <memory>
 #include <map>
+
+struct Job;
 
 /**
  * @class JobTable
@@ -47,95 +48,68 @@ public:
     /**
      * @description: destructor.
      */
-    ~JobTable();
+    ~JobTable() = default;
 
     /**
      * @description: queues a job into the appropriate job list.
-     * @param index used to identify the correct job queue.
+     * @param key worker id string.
      * @param job the job to be queued.
      */
-    void queueJob(unsigned, std::shared_ptr<Job>);
-
-    /**
-     * @description: acquires a job from a given job queue.
-     * @param index the job list from which a job should be pulled.
-     * @param isLocked whether or not the mutex has already been locked.
-     * @returns a pointer to the next job from a given job list. Returns
-     * null if the job list is empty.
-     */
-    std::shared_ptr<Job> acquireJob(unsigned, bool);
+    void queueJob(std::string, std::shared_ptr<Job>);
 
     /**
      * @description: acquires all jobs from a given job queue.
-     * @param the job list from which we shall pull from.
+     * @param key worker id string.
      * @param isLocked whether or not the mutex has already been locked.
      * @returns a safe pointer to a list of pulled jobs. Returns
      * an empty list of there are no jobs to pull.
      */
-    std::shared_ptr<std::vector<std::shared_ptr<Job>>> acquireAllJobs(unsigned, bool);
+    std::shared_ptr<std::vector<std::shared_ptr<Job>>> acquireJobs(std::string, bool);
 
     /**
      * @description: checks a given job flag to see if there is 
      * work to be done.
-     * @param index points to a specific job flag.
+     * @param key worker id string.
      * @returns true if there is work to be done, false otherwise.
      */
-    bool workToBeDone(unsigned);
+    bool workToBeDone(std::string);
 
     /**
      * @description: Accessor method for a given condition variable.
-     * @param index points to a particular condition variable.
+     * @param key worker id string.
      * @returns a pointer to a given condition variable; returns nullptr
      * if the provided index is invalid.
      */
-    std::shared_ptr<std::condition_variable> getCV(unsigned);
+    std::condition_variable &getCV(std::string);
 
     /**
      * @description: Accessor method for a given mutex.
-     * @param index points to a particular mutex.
+     * @param key worker id string.
      * @returns a pointer to a given mutex; returns nullptr
      * if the provided index is invalid.
      */
-    std::shared_ptr<std::mutex> getMutex(unsigned);
-
-    /**
-     * @description: waiter count accessor.
-     * @returns the # of waiters in the restaurant.
-     */
-    inline const unsigned waiterCount() const { return numWaiters_; }
-
-    /**
-     * @description: cook count accessor.
-     * @returns the # of cooks in the restaurant.
-     */
-    inline const unsigned cookCount() const { return numCooks_; }
-
-    /**
-     * @description: bartender count accessor.
-     * @returns the # of bartenders in the restaurant.
-     */
-    inline const unsigned bartenderCount() const { return numBartenders_; }
+    std::mutex &getMutex(std::string);
 
 private:
-    unsigned numWaiters_;                                                ///< # of restaurant waiters
-    unsigned numCooks_;                                                  ///< # of restaurant cooks
-    unsigned numBartenders_;                                             ///< # of restaurant bartenders
-    std::map<unsigned, std::shared_ptr<std::condition_variable>> cvMap_; ///< worker CVs
-    std::vector<std::queue<std::shared_ptr<Job>>> jobQueues_;            ///< worker job queues
-    std::map<unsigned, std::shared_ptr<std::mutex>> mMap_;               ///< job queue mutexes
-    std::vector<bool> jobFlags_;                                         ///< job flags
+    mutable std::map<std::string, std::shared_ptr<WorkerJobData>> dataMap_; ///< worker data map; contains CVs, mutices, job queues, and flags.
 
     /**
      * @description: map initializer function.
-     * Call once at the beginning of simulation.
+     * @param numWaiters # of restaurant waiters
+     * @param numCooks # of restaurant cooks
+     * @param numBartenders # of restaurant bartenders
      */
-    void buildMaps();
+    void buildMap(unsigned = 0, unsigned = 0, unsigned = 0);
+
+    /**
+     * @description: map key validator.
+     * @param key worker id string.
+     * @returns true if key-value mapping exists, false otherwise.
+     */
+    bool validateKey(std::string);
 };
 #endif // JOBTABLE_HPP
 
 /**
- * TODO write methods validateJobQueueSize(unsigned) and validateIndex(unsigned)
- * to eliminate repeated code.
  * TODO make JobTable uncopyable, yet moveable.
- * TODO eliminate the need for worker tallies; use strings as map keys instead.
  */
--- cpp/src/contain/JobTable.cpp
+++ cpp/src/contain/JobTable.cpp
@@ -10,132 +10,102 @@
 #include <iostream>
 
 JobTable::JobTable(unsigned numWaiters, unsigned numCooks, unsigned numBartenders)
-    : numWaiters_(numWaiters),
-      numCooks_(numCooks),
-      numBartenders_(numBartenders),
-      cvMap_(),
-      jobQueues_(numWaiters_),
-      mMap_(),
-      jobFlags_(numWaiters_, false)
+    : dataMap_()
 {
-    buildMaps();
+    buildMap(numWaiters, numCooks, numBartenders);
 }
 
 JobTable::JobTable(const JobTable &jt)
-    : numWaiters_(jt.numWaiters_),
-      numCooks_(jt.numCooks_),
-      numBartenders_(jt.numBartenders_),
-      cvMap_(jt.cvMap_),
-      jobQueues_(jt.jobQueues_),
-      mMap_(jt.mMap_),
-      jobFlags_(jt.jobFlags_) {}
+    : dataMap_(jt.dataMap_) {}
 
 JobTable &JobTable::operator=(const JobTable &jt)
 {
     if (this == &jt)
         return *this;
-    numWaiters_ = jt.numWaiters_;
-    numCooks_ = jt.numCooks_;
-    numBartenders_ = jt.numBartenders_;
-    cvMap_ = jt.cvMap_;
-    jobQueues_ = jt.jobQueues_;
-    mMap_ = jt.mMap_;
-    jobFlags_ = jt.jobFlags_;
+    dataMap_ = jt.dataMap_;
     return *this;
 }
 
-JobTable::~JobTable()
-{ //NOTE explicit clearing may not be necessary. Worth investigating.
-    mMap_.clear();
-    cvMap_.clear();
-    jobQueues_.clear();
-    jobFlags_.clear();
-}
-
-void JobTable::queueJob(unsigned index, std::shared_ptr<Job> job)
+void JobTable::queueJob(std::string key, std::shared_ptr<Job> job)
 {
-    if (index >= numWaiters_) //TODO throw an exception here.
+    if (!validateKey(key)) //TODO throw an exception here.
         return;
     { //begin critical section
-        std::lock_guard<std::mutex> lg(*(mMap_[index]));
-        jobQueues_[index].push(job);
-        jobFlags_[index] = true;
+        std::lock_guard<std::mutex> lg(dataMap_[key]->m_);
+        dataMap_[key]->jobs_.push(job);
+        dataMap_[key]->workToBeDone_ = true;
     } //end critical section
 }
 
-std::shared_ptr<Job> JobTable::acquireJob(unsigned index, bool isLocked)
+std::shared_ptr<std::vector<std::shared_ptr<Job>>> JobTable::acquireJobs(std::string key, bool isLocked)
 {
-    assert(index < numWaiters_ && jobQueues_[index].size() > 0);
-    std::shared_ptr<Job> job;
-    if (isLocked)
-    {
-        job = jobQueues_[index].front();
-        jobQueues_[index].pop();
-    }
-    else
-    {
-        { //begin critical section
-            std::lock_guard<std::mutex> lg(*(mMap_[index]));
-            job = jobQueues_[index].front();
-            jobQueues_[index].pop();
-        } //end critical section
-    }
-
-    return job;
-}
-
-std::shared_ptr<std::vector<std::shared_ptr<Job>>> JobTable::acquireAllJobs(unsigned index, bool isLocked)
-{
-    assert(index < numWaiters_ && jobQueues_[index].size() > 0);
+    if (!validateKey(key)) //TODO throw an exception here.
+        return;
     std::vector<std::shared_ptr<Job>> jobs;
     if (isLocked)
     { //NOTE put repeated code into a single private method!!
-        while (!jobQueues_[index].empty())
+        while (!dataMap_[key]->jobs_.empty())
         {
-            std::shared_ptr<Job> job = jobQueues_[index].front();
-            jobQueues_[index].pop();
+            std::shared_ptr<Job> job = dataMap_[key]->jobs_.front();
+            dataMap_[key]->jobs_.pop();
             jobs.push_back(job);
         }
-        jobFlags_[index] = false;
+        dataMap_[key]->workToBeDone_ = false;
     }
     else
     {
         { //begin critical section
-            std::lock_guard<std::mutex> lg(*(mMap_[index]));
-            while (!jobQueues_[index].empty())
+            std::lock_guard<std::mutex> lg(dataMap_[key]->m_);
+            while (!dataMap_[key]->jobs_.empty())
             {
-                std::shared_ptr<Job> job = jobQueues_[index].front();
-                jobQueues_[index].pop();
+                std::shared_ptr<Job> job = dataMap_[key]->jobs_.front();
+                dataMap_[key]->jobs_.pop();
                 jobs.push_back(job);
             }
-            jobFlags_[index] = false;
+            dataMap_[key]->workToBeDone_ = false;
         } //end critical section
     }
     return std::make_shared<std::vector<std::shared_ptr<Job>>>(jobs);
 }
 
-bool JobTable::workToBeDone(unsigned index)
+bool JobTable::workToBeDone(std::string key)
 {
-    return index < jobQueues_.size() && !jobQueues_[index].empty();
+
+    return validateKey(key) && dataMap_[key]->workToBeDone_;
 }
 
-std::shared_ptr<std::condition_variable> JobTable::getCV(unsigned index)
+std::condition_variable &JobTable::getCV(std::string key)
 {
-    assert(index < numWaiters_);
-    return cvMap_[index];
+    assert(validateKey(key));
+    return dataMap_[key]->cv_;
 }
 
-std::shared_ptr<std::mutex> JobTable::getMutex(unsigned index)
+std::mutex &JobTable::getMutex(std::string key)
 {
-    assert(index < numWaiters_);
-    return mMap_[index];
+    assert(validateKey(key));
+    return dataMap_[key]->m_;
 }
 
-void JobTable::buildMaps()
+void JobTable::buildMap(unsigned numWaiters, unsigned numCooks, unsigned numBartenders)
 {
-    for (int i = 0; i < numWaiters_; ++i)
+    std::string key = "W-";
+    for (int i = 0; i < numWaiters; ++i)
     {
-        cvMap_.emplace(i, std::make_shared<std::condition_variable>());
-        mMap_.emplace(i, std::make_shared<std::mutex>());
+        dataMap_.emplace(key + std::to_string(i), std::make_shared<WorkerJobData>());
     }
+    key = "C-";
+    for (int i = 0; i < numCooks; ++i)
+    {
+        dataMap_.emplace(key + std::to_string(i), std::make_shared<WorkerJobData>());
+    }
+    key = "B-";
+    for (int i = 0; i < numBartenders; ++i)
+    {
+        dataMap_.emplace(key + std::to_string(i), std::make_shared<WorkerJobData>());
+    }
+}
+
+bool JobTable::validateKey(std::string key)
+{
+    return dataMap_.find(key) != dataMap_.end();
 }
