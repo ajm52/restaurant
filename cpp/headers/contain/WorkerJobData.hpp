9df439b3c85bda7601ffae97e02738d0b40aa3b0
AJ <andrew.mcfarlane@ucalgary.ca>
2020-06-30 11:02:37 -0700

    removed copy sems; added comments

--- cpp/headers/contain/WorkerJobData.hpp
+++ cpp/headers/contain/WorkerJobData.hpp
@@ -12,11 +12,25 @@ struct Job;
  * @struct WorkerJobData
  * @author ajm
  * @description: A struct to be used as a map value in JobTable.
- * Contains data related to assigning/acquiring work.
+ * Contains data related to assigning/acquiring work: a job queue, mutex/CV for assigning/acquiring said jobs, and a flag.
  */
 struct WorkerJobData
 {
+    /**
+     * @description: constructor.
+     */
     WorkerJobData() : workToBeDone_(false), m_(), cv_(), jobs_() {}
+
+    /**
+     * @description: destructor.
+     */
+    ~WorkerJobData() = default;
+
+    WorkerJobData(const WorkerJobData &) = delete; ///< WorkerJobData is neither copyable nor movable.
+    WorkerJobData &operator=(const WorkerJobData &) = delete;
+    WorkerJobData(WorkerJobData &&) = delete;
+    WorkerJobData &operator=(WorkerJobData &&) = delete;
+
     mutable bool workToBeDone_;             ///< worker job flag.
     mutable std::mutex m_;                  ///< worker mutex.
     mutable std::condition_variable cv_;    ///< worker condition variable.
